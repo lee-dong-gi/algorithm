ed889e290dd40e42e0e7019a510ecca39bb38768
lee-dong-gi <oksk4753@gmail.com>
2024-09-03 15:31:30 +0900

    feat: binary search comment

--- src/main/kotlin/com/practice/algorithm/AlgorithmService.kt
+++ src/main/kotlin/com/practice/algorithm/AlgorithmService.kt
@@ -9,6 +9,9 @@ class AlgorithmService {
         return if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
     }
 
+    // 미리 정렬되어 있는 경우에 가장 적합
+    // 대량의 데이터에는 유용하나 적은 데이터에서는 비효율적
+    // 요약 : 찾고자하는 값이 배열의 중앙값이면 탐색 완료된다. 배열의 중앙값보다 작으면 중앙값 이전, 크면 중앙값 다음 배열에서 또 다시 중앙값을 찾아 탐색함
     fun binarySearch(targetOrder: Int): UserInfo {
         val userArr = UserInfo.createDummyList(100)
         return binarySearchRecursive(userArr, targetOrder, 0, userArr.size - 1)
@@ -19,14 +22,19 @@ class AlgorithmService {
             return UserInfo(-1, "", -1) // Base case: Target 이 배열에 없는 경우
         }
 
-        val mid = left + (right - left) / 2
+        val mid = left + (right - left) / 2  // 배열의 중앙을 찾음
         val order: Int? = userArr[mid].order
 
         order?.let {
             return when {
-                order == target -> userArr[mid]
-                order < target -> binarySearchRecursive(userArr, target, mid + 1, right)
-                else -> binarySearchRecursive(userArr, target, left, mid - 1)
+                order == target -> userArr[mid]  // 찾고자하는 값이면 반환
+                order < target -> binarySearchRecursive(
+                    userArr,
+                    target,
+                    mid + 1,
+                    right
+                ) // 대상 값이 중앙보다 작으면 현재 중앙 기준 이후 값을 탐색
+                else -> binarySearchRecursive(userArr, target, left, mid - 1) // 대상 값이 중앙보다 크다면 현재 중앙 기준 이전 값을 탐색
             }
         } ?: run {
             throw RuntimeException("order 가 없는 객체 발생")
